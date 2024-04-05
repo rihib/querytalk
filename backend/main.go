cad98bb3bb843707a03986bcc344fe0385d0a4bf
rihito.bannai <rihib@rihib.dev>
2024-04-05 14:17:48 +0900

    refactor: separate handler from main.go

--- backend/main.go
+++ backend/main.go
@@ -1,34 +1,11 @@
 package main
 
 import (
-	"context"
 	"log/slog"
 	"net/http"
 
 	ogen "querytalk/ogen"
 )
-
-type talkService struct{}
-type BadRequestError struct{}
-
-func (e BadRequestError) Error() string {
-	return "Bad Request"
-}
-
-func (s *talkService) SendPrompt(ctx context.Context, req ogen.OptPrompt) (*ogen.VisualizableData, error) {
-	var res ogen.VisualizableData
-
-	if !req.Set {
-		return &res, BadRequestError{}
-	}
-
-	res.VisualizableData = getVisualizableData(req.Value.Prompt)
-	return &res, nil
-}
-
-func (s *talkService) NewError(ctx context.Context, err error) *ogen.ErrorStatusCode {
-	return &ogen.ErrorStatusCode{StatusCode: 500, Response: ogen.Error{Message: err.Error()}}
-}
 
 func main() {
 	service := &talkService{}
