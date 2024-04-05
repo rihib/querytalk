d2edf6418dc5686556ddbd62edabdd964c8be162
rihib <rihib@rihib.dev>
2024-04-05 21:21:17 +0900

    feat: Add openapi function

--- controller/go.mod
+++ controller/go.mod
@@ -5,7 +5,9 @@ go 1.21.5
 require (
 	github.com/go-faster/errors v0.7.1
 	github.com/go-faster/jx v1.1.0
+	github.com/joho/godotenv v1.5.1
 	github.com/ogen-go/ogen v1.0.0
+	github.com/sashabaranov/go-openai v1.20.4
 	go.opentelemetry.io/otel v1.24.0
 	go.opentelemetry.io/otel/metric v1.24.0
 	go.opentelemetry.io/otel/trace v1.24.0
--- controller/logic.go
+++ controller/logic.go
@@ -1,19 +1,58 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func gpt4(prompt string) (string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	OPENAI_API_KEY := os.Getenv("OPENAI_API_KEY")
+
+	client := openai.NewClient(OPENAI_API_KEY)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT4,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: prompt,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("ChatCompletion error: %v", err)
+	}
+
+	return resp.Choices[0].Message.Content, nil
+}
+
 func prompt2sql(prompt string) string {
-	// TODO: Implement this function
-	sql := prompt
+	sql, ok := gpt4(prompt)
+	if ok != nil {
+		return "Error: Could not generate SQL"
+	}
 	return sql
 }
 
 func getData(sql string) string {
-	// TODO: Implement this function
 	data := sql
 	return data
 }
 
 func createVisualizableData(data string) string {
-	// TODO: Implement this function
 	visualizableData := data
 	return visualizableData
 }
